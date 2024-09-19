8dec3d68b21d1fed150a88c8cd75831f3deb1c9c
lilycherly <lilycherly@localhost.localdomain>
2024-09-19 15:31:21 +0300

    add  RegisterService

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 #include <QDBusInterface>
 #include <QDBusMetaType>
 #include <QDebug>
-
+#include <QFile>
+#include <QVector>
 class SharingService : public QObject {
     Q_OBJECT
     Q_CLASSINFO("D-Bus Interface", "com.system.sharing");
@@ -26,19 +27,57 @@ public:
             exit(1);
         }
     }
+public Q_SLOTS:   
+    QString RegisterService(const QString &name, const QStringList supportedFormats){
+        
+	    //В config.txt  будем сохранят информацию о сервисах
+	    QFile configFile("config.txt");
+	    //  Открываем файл чтобы записать туда информацию
+	    //  если файл не открылся выводим информацию об ошибке 
+            if (!configFile.open(QIODevice::Append | QIODevice::Text)){
+		    QString errmess =  "Cannot open the file \"config.txt\""; 
+		    qDebug() << errmess;
+		    return  errmess;
+            }
+	    
+	    //Считываем сервисы чтобы проверить на наличие дубликатов
+	    QStringList existingServices;
+	    QString line;
+	    while (line = configFile.readLine(), !line.isNull()) {
+           	 if (line.startsWith("Service Name: ")) {
+                	existingServices << line.mid(14).trimmed();
+           	 }
+            }
 
-public Q_SLOTS:
-    QString exampleMethod(const QString &input) {
-        qDebug() << "exampleMethod called" << input;
-	qDebug() << "Hello world!" + input;
-        return input;
-    }
+	    // Проверяем на наличие дубликатов
+            if (existingServices.contains(name)) {
+            	QString mess =  "Сервис с именем"+name +" уже зарегистрирован.";
+		qDebug() << mess;
+		return mess;
+            }
+
+            // Перемещаем указатель в конец файла
+            configFile.seek(0);
+
+	    //Записываем информацию о сервисе
+	    QTextStream out(&configFile);
+	    out << "Supported service: "<< name <<"\n";
+	    out << "Supported formats : ";
+	    for( const QString& str : supportedFormats){
+		 out << str << ",";
+	    }
+	    out << "\n";
+           
+	    return nullptr;
+   }
 };
 
 int main(int argc, char *argv[]) {
     QCoreApplication app(argc, argv);
 
     SharingService service;
+
+    qDebug() << "QDBus сервис запущен. Ожидание вызовов...";
 
     return app.exec();
 }
